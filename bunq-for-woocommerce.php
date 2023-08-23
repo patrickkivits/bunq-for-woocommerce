89fb7849513714dc3c3d579113c58e644c2586ce
Patrick Kivits <patrick@patrickkivits.nl>
2023-08-23 08:44:38 +0200

    Bump version

--- bunq-for-woocommerce.php
+++ bunq-for-woocommerce.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: bunq for WooCommerce
  * Description: Accept payments in your WooCommerce shop with just your bunq account.
- * Version: 1.2.6
+ * Version: 1.2.7
  * Author: Patrick Kivits
  * Author URI: https://www.patrickkivits.nl
  * Requires at least: 3.8
